c1040f4d3b8697df58ab602a07eb8526aa6a471d
frantesting001@gmail.com <rohitk123>
2015-09-20 22:17:40 +0530

    1st changes

---
+++ AutoGmail/src/com/gmail/generichandler/Browser.java
@@ -0,0 +1,7 @@
+package com.gmail.generichandler;
+
+public class Browser {
+	public static void main(String arg[]){
+		System.out.println("Browser Method!");
+	}
+}
---
+++ AutoGmail/src/com/gmail/generichandler/Excel.java
@@ -0,0 +1,7 @@
+package com.gmail.generichandler;
+
+public class Excel {
+	public static void main(String arg[]){
+		System.out.println("Excel Methods");
+	}
+}
