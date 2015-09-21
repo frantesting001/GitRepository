d19168fa7da818b08a96dedb495598d35595eeb5
frantesting001@gmail.com <rohitk123>
2015-09-21 07:31:24 +0530

    201509210731

--- AutoGmail/src/com/gmail/generichandler/StringHandler.java
+++ AutoGmail/src/com/gmail/generichandler/StringHandler.java
@@ -1,5 +1,25 @@
 package com.gmail.generichandler;
 
-public class StringHandler {
+import org.testng.annotations.Test;
 
+public class StringHandler {
+	
+	public static String createStringObj(char[] charsobj){
+		String strobj=new String(charsobj); //creating new object
+		System.out.println(strobj);
+		return strobj;
+	}
+	
+	public static void stringArray2D(){
+		char[] charobj={'a','b','c','d'}; //creating new object
+		System.out.println(charobj[2]);
+	}
+	@Test
+	public static void newCharObj(){
+		char[] charobj={'a','b','c','d'}; //creating new object
+		String strobj=new String(charobj,2,2);
+		System.out.println(strobj);
+	}
+	
+	
 }
